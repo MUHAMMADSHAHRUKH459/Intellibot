c1efa8b1fd97a61013eb76cc85968f9e90778417
MuhammadS-Shahrukh <158289971+MUHAMMADSHAHRUKH459@users.noreply.github.com>
2025-02-06 03:20:13 +0500

    Add chatbot

--- src/components/chatbots/chatbot.tsx
+++ src/components/chatbots/chatbot.tsx
@@ -3,44 +3,31 @@
 import { useEffect } from "react";
 
 declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "df-messenger": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-        "chat-icon"?: string;
-        "intent"?: string;
-        "chat-title"?: string;
-        "agent-id"?: string;
-        "language-code"?: string;
-        "chat-background"?: string;
-        "button-titlebar-color"?: string;
-        "button-color"?: string;
-      };
-    }
+  interface Window {
+    kommunicate: any;
   }
 }
 
 const Chatbot = () => {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1";
-    script.async = true;
-    document.body.appendChild(script);
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "32a6874456e548d787d56bcc4e5d87882",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
   }, []);
 
-  return (
-    <>
-      <df-messenger
-        chat-icon="images\logo\chat.jpg"
-        intent="WELCOME"
-        chat-title="Intellibot-Assistant"
-        agent-id="14243804-411a-4707-8306-e99d159c9061"
-        language-code="en"
-        chat-background="#ffffff"
-        button-titlebar-color="#000"
-        button-color="#ffffff"
-      ></df-messenger>
-    </>
-  );
+  return <></>;
 };
 
 export default Chatbot;
