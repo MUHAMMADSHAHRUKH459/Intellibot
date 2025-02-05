c9c5fe26211459c010b2cf9b98558083b88d4413
MuhammadS-Shahrukh <158289971+MUHAMMADSHAHRUKH459@users.noreply.github.com>
2025-02-05 16:32:35 +0500

    Add chatbot

--- src/components/chatbots/chatbot.tsx
+++ src/components/chatbots/chatbot.tsx
@@ -30,7 +30,7 @@ const Chatbot = () => {
   return (
     <>
       <df-messenger
-        chat-icon="https://files.oaiusercontent.com/file-FB7dumLWwoQ858ny1deisQ?se=2025-02-05T11%3A04%3A14Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D50bb4971-e883-4a19-bb09-0b34e090c5d8.webp&sig=hu17Is8dhjqn8qaE3f69hAv%2B5xjkYExr%2BHwQkDm5tb0%3D"
+        chat-icon="images\logo\chat.jpg"
         intent="WELCOME"
         chat-title="Intellibot-Assistant"
         agent-id="14243804-411a-4707-8306-e99d159c9061"
