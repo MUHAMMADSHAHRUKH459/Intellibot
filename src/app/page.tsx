7c4099afabc2699e4a5412b3b9df97430c39680f
ahmedraza <aaza60232@gmail.com>
2025-03-08 02:07:16 +0500

    meta pixel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+import { useEffect } from "react";
+import Script from "next/script";
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 // import Blog from "@/components/Blog";
@@ -13,24 +16,48 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Intellibot Creation",
-  description: "This is AI Automation company",
-  // other metadata
+  description: "This is an AI Automation company",
 };
 
+const PIXEL_ID = "664248859624021";
+
 export default function Home() {
+  useEffect(() => {
+    window.fbq?.("init", PIXEL_ID);
+    window.fbq?.("track", "PageView");
+  }, []);
   return (
     <>
+      {/* Meta Pixel Script */}
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${PIXEL_ID}');
+          fbq('track', 'PageView');
+        `,
+        }}
+      />
       <ScrollUp />
       <Hero />
       <Features />
       {/* <Video /> */}
-       {/* <Brands /> */}
+      {/* <Brands /> */}
       {/* <AboutSectionOne /> */}
       <AboutSectionTwo />
       <Testimonials />
       {/* <Pricing /> */}
       {/* <Blog /> */}
       {/* <Contact /> */}
-    </> 
+    </>
   );
 }
