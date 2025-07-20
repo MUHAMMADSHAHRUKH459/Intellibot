4ccf36f27b0e865c1d07235ac777fb96f6b62fec
MuhammadS-Shahrukh <158289971+MUHAMMADSHAHRUKH459@users.noreply.github.com>
2025-07-20 05:36:09 +0500

    Update komunicate.tsx

--- src/components/chatbots/komunicate.tsx
+++ src/components/chatbots/komunicate.tsx
@@ -1,28 +1,29 @@
-// components/KommunicateChat.js
+"use client";
 
-import Script from 'next/script';
+import { useEffect } from "react";
 
 const KommunicateChat = () => {
-  return (
-    <>
-      <Script
-        id="kommunicate-chat-widget"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(d, m){
-              var kommunicateSettings = 
-                {"appId":"29713958bd664aecb4b36fc7e4a37cf0c","popupWidget":true,"automaticChatOpenOnNavigation":true};
-              var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-              s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-              var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-              window.kommunicate = m; m._globals = kommunicateSettings;
-            })(document, window.kommunicate || {});
-          `,
-        }}
-      />
-    </>
-  );
+  useEffect(() => {
+    (function (d, m) {
+      const kommunicateSettings = {
+        appId: "2ab527cfad03d9769414a910684bac87c",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+
+      const s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      const h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+
+      (window as any).kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
+  }, []);
+
+  return null; // No visual UI needed, it just injects the script
 };
 
 export default KommunicateChat;
