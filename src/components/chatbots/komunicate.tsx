8c27dd71bd3b85b45d66b8a49a7190aecdf98dc1
MuhammadS-Shahrukh <158289971+MUHAMMADSHAHRUKH459@users.noreply.github.com>
2025-07-20 06:04:55 +0500

    Update komunicate.tsx

--- src/components/chatbots/komunicate.tsx
+++ src/components/chatbots/komunicate.tsx
@@ -1,30 +1,27 @@
+// app/components/KommunicateChat.tsx
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 
 const KommunicateChat = () => {
-  useEffect(() => {
-    (function (d, m) {
-      const kommunicateSettings = {
-        appId: "2ab527cfad03d9769414a910684bac87c",
-        popupWidget: true,
-        automaticChatOpenOnNavigation: true,
-      };
-
-      const s = document.createElement("script");
-      s.type = "text/javascript";
-      s.async = true;
-      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-
-      const h = d.getElementsByTagName("head")[0];
-      h.appendChild(s);
-
-      (window as any).kommunicate = m;
-      m._globals = kommunicateSettings;
-    })(document, (window as any).kommunicate || {});
-  }, []);
-
-  return null; // Yeh component sirf script inject karega
+  return (
+    <Script
+      id="kommunicate-chat"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(d, m){
+              var kommunicateSettings = 
+                  {"appId":"2ab527cfad03d9769414a910684bac87c","popupWidget":true,"automaticChatOpenOnNavigation":true};
+              var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+              s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+              var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+              window.kommunicate = m; m._globals = kommunicateSettings;
+          })(document, window.kommunicate || {});
+        `,
+      }}
+    />
+  );
 };
 
 export default KommunicateChat;
