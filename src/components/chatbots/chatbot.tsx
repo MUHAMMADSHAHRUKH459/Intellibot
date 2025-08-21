473fe32acefd56e06f58e2fa11cff17bf6a70d61
MuhammadS-Shahrukh <158289971+MUHAMMADSHAHRUKH459@users.noreply.github.com>
2025-08-22 04:16:28 +0500

    Update chatbot.tsx

--- src/components/chatbots/chatbot.tsx
+++ src/components/chatbots/chatbot.tsx
@@ -1,27 +1,28 @@
-// app/components/KommunicateChat.tsx
+// components/KommunicateChat.js
 "use client";
-
 import Script from "next/script";
 
-const KommunicateChat = () => {
+export default function KommunicateChat() {
   return (
-    <Script
-      id="kommunicate-chat"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function(d, m){
+    <>
+      <Script
+        id="kommunicate-chat"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(d, m){
               var kommunicateSettings = 
-                  {"appId":"2ab527cfad03d9769414a910684bac87c","popupWidget":true,"automaticChatOpenOnNavigation":true};
+                  {"appId":"841c3551affcc2e51810a068d7c00c19","popupWidget":true,"automaticChatOpenOnNavigation":true};
               var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
               s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
               var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
               window.kommunicate = m; m._globals = kommunicateSettings;
-          })(document, window.kommunicate || {});
-        `,
-      }}
-    />
+            })(document, window.kommunicate || {});
+          `,
+        }}
+      />
+    </>
   );
-};
+}
 
-export default KommunicateChat;
+
